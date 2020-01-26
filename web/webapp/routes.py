5d0abd0e56729a9c6d8db92f63193539c1b69faa
stijn.pieters@student.howest.be <stijn.pieters@student.howest.be>
2020-01-26 20:42:32 +0100

    updated routes.py

--- web/webapp/routes.py
+++ web/webapp/routes.py
@@ -18,3 +18,36 @@ def get_data():
         result = client_influx.query(query, bind_params={'serial': serial_number})
         return result.raw["series"][0]
 
+
+@app.route('/electricityconsumption', methods=["GET"])
+def get_consumption():
+    if request.method == "GET":
+        serial_number = request.args["serial"]
+
+        today = datetime.date.today()
+        dt = datetime.datetime(today.year, today.month, 1)
+        startofmonth = time.mktime(dt.timetuple())
+
+        measurement = "Actueel_vermogen_uit_net"
+        client_influx = InfluxDBClient("35.233.68.4", 8086)
+        client_influx.switch_database("demodb")
+        query = 'select sum("Waarde") from {0} WHERE (Serienummer_meter=$serial) AND time >= $startofmonth'.format(measurement)
+        result = client_influx.query(query, bind_params={'serial': serial_number, 'startofmonth': startofmonth})
+        return result.raw["series"][0]
+
+
+@app.route('/solarpanelyield', methods=["GET"])
+def get_production():
+    if request.method == "GET":
+        serial_number = request.args["serial"]
+
+        today = datetime.date.today()
+        dt = datetime.datetime(today.year, today.month, 1)
+        startofmonth = time.mktime(dt.timetuple())
+
+        measurement = "Actueel_vermogen_naar_net"
+        client_influx = InfluxDBClient("35.233.68.4", 8086)
+        client_influx.switch_database("demodb")
+        query = 'select "Waarde" from {0} WHERE (Serienummer_meter=$serial) AND time >= $startofmonth'.format(measurement)
+        result = client_influx.query(query, bind_params={'serial': serial_number, 'startofmonth': startofmonth})
+        return result.raw["series"][0]

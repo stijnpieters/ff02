2768423890e7fc426d53b0ae3536bc9342ad2dd2
stijn.pieters@student.howest.be <stijn.pieters@student.howest.be>
2020-01-26 21:39:35 +0100

    updated routes.py

--- web/webapp/routes.py
+++ web/webapp/routes.py
@@ -8,57 +8,41 @@ from influxdb import InfluxDBClient
 from random import randint
 
 
-@app.route('/data', methods=["GET"])
-def get_data():
-    if request.method == "GET":
-        serial_number = request.args["serial"]
-        measurement = request.args["measurement"]
-        client_influx = InfluxDBClient("35.233.68.4", 8086)
-        client_influx.switch_database("demodb")
-        query = 'select "Waarde" from {0} WHERE (Serienummer_meter=$serial) AND time >= now()- 30m'.format(measurement)
-        result = client_influx.query(query, bind_params={'serial': serial_number})
-        return {'values': result.raw["series"][0]["values"]}
-
-
 @app.route('/electricityconsumption', methods=["GET"])
 def get_consumption():
     if request.method == "GET":
         serial_number = request.args["serial"]
-
-        today = datetime.date.today()
-
-        measurement = "Actueel_vermogen_uit_net"
-        client_influx = InfluxDBClient("35.233.68.4", 8086)
-        client_influx.switch_database("demodb")
-
-        query = 'select sum("Waarde") from {0} WHERE (Serienummer_meter=$serial) AND time >= now() - {1}d'.format(measurement, today.day - 1)
-        result = client_influx.query(query, bind_params={'serial': serial_number})
-        return {'values': result.raw["series"][0]["values"]}
+        return get_data(serial_number, "Actueel_vermogen_uit_net")
 
 
 @app.route('/solarpanelyield', methods=["GET"])
-def get_production():
+def solarpanelyield():
     if request.method == "GET":
         serial_number = request.args["serial"]
+        return get_data(serial_number, "Actueel_vermogen_naar_net")
 
-        today = datetime.date.today()
 
-        measurement = "Actueel_vermogen_naar_net"
-        client_influx = InfluxDBClient("35.233.68.4", 8086)
-        client_influx.switch_database("demodb")
-        query = 'select sum("Waarde") from {0} WHERE (Serienummer_meter=$serial) AND time >= now() - {1}d'.format(measurement, today.day - 1)
-        result = client_influx.query(query, bind_params={'serial': serial_number})
-        return {'values': result.raw["series"][0]["values"]}
+def get_data(serial, measurement):
+    today = datetime.date.today()
+
+    client_influx = InfluxDBClient("35.233.68.4", 8086)
+    client_influx.switch_database("demodb")
+    query = 'select sum("Waarde") from {0} WHERE (Serienummer_meter=$serial) AND time >= now() - {1}d'.format(
+        measurement, today.day - 1)
+    result = client_influx.query(query, bind_params={'serial': serial})
+    return {'values': result.raw["series"][0]["values"]}
 
 
 @app.route('/import', methods=["GET"])
 def get_imports():
-    consumption = get_consumption()["values"][0][1]
-    production = get_production()["values"][0][1]
-    if consumption - production < 0:
-        return abs(consumption - production)
-    else:
-        return 0
+    if request.method == "GET":
+        serial = request.args["serial"]
+        consumption = get_data(serial, "Actueel_vermogen_van_net")["values"][0][1]
+        production = get_data(serial, "Actueel_vermogen_naar_net")["values"][0][1]
+        if consumption - production < 0:
+            return abs(consumption - production)
+        else:
+            return 0
 
 
 @app.route('/export', methods=["GET"])

9a89a45a5f59abefec4b2bf782c2bc0187577950
stijn.pieters@student.howest.be <stijn.pieters@student.howest.be>
2020-01-26 21:06:42 +0100

    updated routes.py

--- web/webapp/routes.py
+++ web/webapp/routes.py
@@ -42,12 +42,10 @@ def get_production():
         serial_number = request.args["serial"]
 
         today = datetime.date.today()
-        dt = datetime.datetime(today.year, today.month, 1)
-        startofmonth = time.mktime(dt.timetuple())
 
         measurement = "Actueel_vermogen_naar_net"
         client_influx = InfluxDBClient("35.233.68.4", 8086)
         client_influx.switch_database("demodb")
-        query = 'select "Waarde" from {0} WHERE (Serienummer_meter=$serial) AND time >= $startofmonth'.format(measurement)
+        query = 'select sum("Waarde") from {0} WHERE (Serienummer_meter=$serial) AND time >= now() - {1}d'.format(measurement, today.day - 1)
         result = client_influx.query(query, bind_params={'serial': serial_number, 'startofmonth': startofmonth})
         return {'values': result.raw["series"][0]["values"]}
